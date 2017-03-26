1045166ce8573fff725ab01ab6d2e1abff159fbf
Maria <35735@ukr.net>
2017-03-26 14:32:19 +0300

    Add_new_task_12

--- src/com/company/TestFromQizful/AmendTheSentence.java
+++ src/com/company/TestFromQizful/AmendTheSentence.java
@@ -10,16 +10,35 @@ amendTheSentence(s) = "hello".*/
 public class AmendTheSentence {
     public static void main(String[] args) {
         String s = "CodefightsIsAwesome";
-
-//        s = s.toLowerCase();
-       System.out.println( amendTheSentence(s));
+        System.out.println(amendTheSentence(s));
 
     }
+
     static String amendTheSentence(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        sb.append(" ");
-        s =s.toLowerCase();
-        return s;
+        String result = " ";
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                result += " " + s.charAt(i);
+            } else if (Character.isLowerCase(s.charAt(i))) {
+                result += s.charAt(i);
+            }
+            result =result.toLowerCase();
+
+
+        }
+        result = result.trim();
+        return result;
 
     }
+//    StringBuilder builder = new StringBuilder();
+//		for (char c : s.toCharArray()) {
+//        if(Character.isUpperCase(c)){
+//            builder.append(" " + Character.toLowerCase(c));
+//        } else{
+//            builder.append("" + c);
+//        }
+//    }
+//		return builder.toString().trim();
+//}
 }
+
