49d85b190b5a29aa6d128818c390d4816948d80e
Maria <35735@ukr.net>
2017-03-28 23:05:30 +0300

    Add_new_task_15

--- src/com/company/TestFromQizful/TestFromQ3.java
+++ src/com/company/TestFromQizful/TestFromQ3.java
@@ -14,39 +14,64 @@ public class TestFromQ3 {
         int[] a = {6, 7, 3, 8};
 
         sort(a);
-        for (int i : a) {
-            System.out.println(i);
-
-        }
-
+//        for (int i : a) {
+//            System.out.println(i);
+//
+//        }
     }
 
+
+
+
+
+
     static void sort(int[] a) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < a.length; i++) {
-            list.add(a[i]);
-
-        }
-      List<Integer> list2 = new ArrayList<>();
-
-        ListIterator<Integer> listIterator = list.listIterator();
-        while (listIterator.hasNext()) {
-
-            for (int i = 0; i < list.size(); i++) {
-                if (listIterator.nextIndex() > list.get(i)) {
-                    list2.add();
-
-
-
-                }
+        for (int i = 0; i <a.length-1 ; i++) {
+            System.out.println(a[i+1]);
 
             }
 
 
+            }
+
         }
 
-    }
-}
+
+
+
+//    static void sort(int[] a) {
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < a.length; i++) {
+//            list.add(a[i]);
+//
+//        }
+//      List<Integer> list2 = new ArrayList<>();
+//        int max = list.get(0);
+//
+//        ListIterator<Integer> listIterator = list.listIterator();
+//        while (listIterator.hasNext()) {
+//
+//
+//            for (int i = 0; i < list.size(); i++) {
+//                if (listIterator.nextIndex() > list.get(i)) {
+//                    max = listIterator.nextIndex();
+//
+//
+//
+//
+//                }
+//
+//
+//            }
+//
+//
+//        }
+//        System.out.println(max);
+//
+//
+//
+//    }
+
 
 
 
