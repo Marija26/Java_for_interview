d2be71fc1967781a4d287a2fbe9787d4f4cd3080
Maria <35735@ukr.net>
2017-03-28 22:40:36 +0300

    Add_new_task_14

--- src/com/company/TestFromQizful/TestFromQ3.java
+++ src/com/company/TestFromQizful/TestFromQ3.java
@@ -1,45 +1,76 @@
 package com.company.TestFromQizful;
 
-/**
- * Created by Маша on 27.03.2017.
- */
+
+
+
+/*For a = [6, 7, 3, 8], the output should be
+  nextLarger(a) = [7, 8, 8, -1].*/
+
+import java.util.*;
+
 public class TestFromQ3 {
 
-   static class A1 {}
+    public static void main(String[] args) {
+        int[] a = {6, 7, 3, 8};
 
-    interface I1 {}
+        sort(a);
+        for (int i : a) {
+            System.out.println(i);
 
-    static class B1 extends A1 implements I1 {}
-
-    public static class OverloadTest {
-        static public  void foo(A1 a) { System.out.print("A"); }
-        static private void foo(B1 b) { System.out.print("B"); }
-        static private void foo(I1 i) { System.out.print("I"); }
-
-        public static void main(String[] args) {
-            A1 a = new B1();
-            OverloadTest.foo(a);
-            OverloadTest.foo((I1) a);
         }
 
     }
 
-//        Object a = new Object();
-//        String strA = "text";
-//        String strB = "text";
-//        strA += "1";
-//        strB += "1";
-//        System.out.println(strA != strB);
-//        strA = "text1";
-//        strB = "text1";
-//        System.out.println(strA != strB);
-//        char ch = '0';
-//        while (ch <= 9)
-//            System.out.print(ch++);
-//        System.out.println(true ? false :true == true ? false : true);
-//        byte b = 0;
-//        while (++b > 0);
-//        System.out.println(b);
+    static void sort(int[] a) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            list.add(a[i]);
+
+        }
+      List<Integer> list2 = new ArrayList<>();
+
+        ListIterator<Integer> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+
+            for (int i = 0; i < list.size(); i++) {
+                if (listIterator.nextIndex() > list.get(i)) {
+                    list2.add();
 
 
+
+                }
+
+            }
+
+
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
