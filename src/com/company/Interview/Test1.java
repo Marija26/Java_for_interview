3c4036054d2a41bb044394b1c5863376e78d6428
Maria <35735@ukr.net>
2017-05-26 18:05:28 +0300

    Add_new_task_34

--- src/com/company/Interview/Test1.java
+++ src/com/company/Interview/Test1.java
@@ -20,17 +20,22 @@ import java.io.InputStreamReader;
 public class Test1 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int c = Integer.parseInt(reader.readLine());
-        System.out.println(number(c));
-    }
-
-    public static int number(int c) {
-        int a = 1;
-        while(a>0){
-        c =a* a ;
+        double a = Integer.parseInt(reader.readLine());
+        System.out.println(sqq((int) a));
 
     }
-        return c;
 
+    public static double sqq(int x) {
+        double num = 1;
+        double rez = 0;
+        while (x > 0) {
+            x -= num;
+            num += 2;
+            if(x<0) return 0;
+            else return 1;
+//            rez += (x < 0) ? 0 : 1;
+        }
+        return rez;
+    }
 }
 
---
+++ src/com/company/TestFromQizful/Test/Test_2.java
@@ -0,0 +1,45 @@
+package com.company.TestFromQizful.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Маша on 26.05.2017.
+ */
+/*
+Задача   2
+Задание Задан массив, элементами которого являются произвольные целые числа. Необходимо
+ определить (максимально оптимальным образом) элемент с индексом k для массива,
+  состоящего из таких же чисел, но отсортированного по возрастанию. Например, для
+  массива   [6,   1,   4,   1,   7]   и   k=3
+  правильным   ответом   будет   число   4   ([1,   1,   4,   6,   7]).
+Входные   данные Массив   произвольных   целых   чисел   и   натуральное
+ число   k   (1   <=   k   <=   длина   массива).
+Выходные   данные k-й элемент массива, состоящего из таких же чисел, как и исходный массив, но
+  отсортированного   по   возрастанию.
+ */
+public class Test_2 {
+    public static void main(String[] args) {
+//        List<Integer> list = new ArrayList<>();
+//        list.add(6);
+//        list.add(1);
+//        list.add(4);
+//        list.add(1);
+//        list.add(7);
+      int[] array = {3,   2,   7,   4,   8};
+        int k = 3;
+        sort(array, k);
+
+    }
+
+    public static void sort(int[]a, int k) {
+        Arrays.sort(a);
+            for (int i= 0; i < a.length; i++) {
+                System.out.println(a[k-1]);
+
+            }
+        }
+    }
+
