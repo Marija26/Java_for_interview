db87b259f78d21b032ccabd450959e0845d0d34d
Maria <35735@ukr.net>
2017-05-27 15:24:54 +0300

    Add_new_task_36

--- src/com/company/TestFromQizful/TestCodeFights.java
+++ src/com/company/TestFromQizful/TestCodeFights.java
@@ -3,6 +3,7 @@ package com.company.TestFromQizful;
 import com.company.Lesson_Threads.Test_21.SleepingThread;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Spliterator;
 import java.util.regex.Matcher;
@@ -25,25 +26,61 @@ and 3 characters to get the output string of "24-a0r-74k".*/
 public class TestCodeFights {
     public static void main(String[] args) {
         String s = "2-4a0r7-4k";
+       s =s.replace("-", "");
         int k = 3;
-        System.out.println(stringReformatting(s, k));
+
+
+//        for (String s2 : s.split(String.valueOf(k), k)) {
+//            System.out.println(s2);
+
+
+        System.out.println(format(s, k));
     }
 
-    static String stringReformatting(String s, int k) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int j = 0;
-        for (int i = s.length()-1; i >0 ; i++) {
-            if()
 
+    public static String formatString(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int counter = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                sb.append(s.charAt(i));
+                counter++;
+            }
+            if (counter == k) {
+                counter = 0;
+                sb.append("-");
+            }
         }
+        return sb.toString();
+
+    }
+
+    public static String format(String str, int k) {
+        int size = str.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = size - 1; i >= 0; i--) {
+            sb.append(str.charAt(i));
+            if ((i + 1) % k == 0)
+                sb.append("-");
+        }
+
+        return sb.reverse().toString();
+    }
+}
+
 
 //
 //        String r = s.replace("-", "");
 //        return (r.substring(0, r.length() % k) + r.substring(r.length() % k).replaceAll(String.format("(.{%d})", k), "-$1")).replaceAll("^-", "");
-    }
 
 
-}
+
+
+
+
+
+
+
 
 
 
